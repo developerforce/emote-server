40edc3c52718a0ea00ce3a0e70047100972d0912
Clifton Campbell <clif@mimio.io>
2020-08-19 13:02:03 -0700

    convert to integer

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 
 const PORT = process.env.PORT || 8080
 const REDIS_URL = process.env.REDIS_URL
-const RATE_LIMIT_MAX = process.env.RATE_LIMIT_MAX || 100
+const RATE_LIMIT_MAX = +process.env.RATE_LIMIT_MAX || 100
 const RATE_LIMIT_WINDOW = process.env.RATE_LIMIT_WINDOW || '1 minute'
 const HEARTBEAT_TIMEOUT = process.env.HEARTBEAT_TIMEOUT || 30
-const EVENT_ID_LENGTH = process.env.EVENT_ID_LENGTH || 32
-const EVENTS_MAX = process.env.EVENTS_MAX || 32
+const EVENT_ID_LENGTH = +process.env.EVENT_ID_LENGTH || 32
+const EVENTS_MAX = +process.env.EVENTS_MAX || 32
 
 const EMOTE_ALLOWLIST = [
   'celebrate',
